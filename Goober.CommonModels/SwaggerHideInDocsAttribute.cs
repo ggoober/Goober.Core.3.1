ff38f2deb917bc9d390f002f6f41b20de2745626
Denis Tskhay <denis.tskhaj@yandex.ru>
2021-04-04 14:14:26 +0300

    common models

---
+++ Goober.CommonModels/SwaggerHideInDocsAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Goober.CommonModels
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SwaggerHideInDocsAttribute : Attribute
+    {
+        public SwaggerHideInDocsAttribute()
+        {
+
+        }
+
+        public SwaggerHideInDocsAttribute(string password)
+        {
+            Password = password;
+        }
+
+        public string Password { get; set; } = "password";
+    }
+}
