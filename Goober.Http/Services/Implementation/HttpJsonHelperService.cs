b6976fd3e11352f15891821787c0b2146a590ce6
Denis Tskhay <denis.tskhaj@yandex.ru>
2021-04-09 23:49:45 +0300

    fix

--- Goober.Http/Services/Implementation/HttpJsonHelperService.cs
+++ Goober.Http/Services/Implementation/HttpJsonHelperService.cs
@@ -9,6 +9,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using Goober.Http.Utils;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
 
 namespace Goober.Http.Services.Implementation
 {
@@ -93,6 +96,8 @@ namespace Goober.Http.Services.Implementation
                     loggingStrContent: string.Empty
                     );
 
+
+
                 var ret = await httpResponse.Content.ReadAsStringAsync();
 
                 return ret;
@@ -162,6 +167,70 @@ namespace Goober.Http.Services.Implementation
             }
         }
 
+        public async Task<TResponse> UploadFileAsync<TResponse>(string url,
+            IFormFile file,
+            string formDataFileParameterName = "file",
+            AuthenticationHeaderValue authenticationHeaderValue = null,
+            List<KeyValuePair<string, string>> headerValues = null,
+            JsonSerializerSettings jsonSerializerSettings = null,
+            int timeoutInMilliseconds = 120000,
+            long responseMaxContentLength = 30 * 1024)
+        {
+            var contentType = file.ContentType;
+
+            using (var fileStream = file.OpenReadStream())
+            {
+                using (var httpClient = _httpClientFactory.CreateClient())
+                {
+                    httpClient.Timeout = TimeSpan.FromMilliseconds(timeoutInMilliseconds);
+
+                    var httpRequest = HttpUtils.GenerateHttpRequestMessage(
+                        requestUri: url,
+                        httpMethodType: HttpMethod.Post,
+                        authenticationHeaderValue: authenticationHeaderValue,
+                        headerValues: null,
+                        responseMediaTypes: new List<string> { ApplicationJsonContentTypeValue });
+
+                    var fileStreamContent = new StreamContent(fileStream);
+                    fileStreamContent.Headers.Add("Content-Type", file.ContentType);
+
+                    var formData = new MultipartFormDataContent();
+                    formData.Add(fileStreamContent, formDataFileParameterName, file.FileName);
+
+                    httpRequest.Content = formData;
+
+                    foreach (var iCustomHeader in headerValues)
+                    {
+                        httpRequest.Content.Headers.Add(iCustomHeader.Key, iCustomHeader.Value);
+                    }
+
+                    var httpResponse = await httpClient.SendAsync(httpRequest);
+
+                    if (httpResponse.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        return default;
+                    }
+
+                    var strContent = $"content: {file.FileName}, headers: {Serialize(headerValues)}";
+
+                    await ThrowExceptionIfResponseIsNotValidAsync(httpResponse: httpResponse,
+                        maxContentLength: responseMaxContentLength,
+                        loggingUrl: url,
+                        loggingStrContent: strContent);
+
+                    var responseStream = await httpResponse.Content.ReadAsStreamAsync();
+                    //ReadResponseWithMaxSizeRetrictionAsync(responseStream, Encoding.UTF8, url, )
+                    var ret = await httpResponse.Content.ReadAsStringAsync();
+                    if (string.IsNullOrEmpty(ret) == true)
+                    {
+                        return default;
+                    }
+
+                    return Deserialize<TResponse>(ret, jsonSerializerSettings);
+                }
+            }
+        }
+
         #region private methods
 
         private static string Serialize(object value, JsonSerializerSettings serializerSettings = null)
@@ -169,9 +238,22 @@ namespace Goober.Http.Services.Implementation
             return JsonConvert.SerializeObject(value, serializerSettings ?? _jsonSerializerSettings);
         }
 
-        private static T Deserialize<T>(string value, JsonSerializerSettings serializerSettings = null)
+        private static T Deserialize<T>(string value, 
+            JsonSerializerSettings serializerSettings,
+            string loggingUrl,
+            AuthenticationHeaderValue authenticationHeaderValue,
+            List<KeyValuePair<string, string>> headerValues)
         {
-            return JsonConvert.DeserializeObject<T>(value, serializerSettings ?? _jsonSerializerSettings);
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(value, serializerSettings ?? _jsonSerializerSettings);
+            }
+            catch (Exception exc)
+            {
+                throw new InvalidOperationException(
+                    message: $"Can't deserialize to type = {typeof(T).Name} from value = {value}, url = {loggingUrl}, content = {loggingStrContent}",
+                    innerException: exc);
+            }
         }
 
         private static async Task ThrowExceptionIfResponseIsNotValidAsync(HttpResponseMessage httpResponse, long maxContentLength, string loggingUrl, string loggingStrContent)
@@ -197,6 +279,68 @@ namespace Goober.Http.Services.Implementation
             }
 
             throw exception;
+        }
+
+        private async Task<string> ReadResponseWithMaxSizeRetrictionAsync(Stream stream, 
+            Encoding encoding,
+            string loggingUrl,
+            string loggingStrContent,
+            int maxSize,
+            int bufferSize = 1024)
+        {
+            if (stream.CanRead == false)
+            {
+                throw new InvalidOperationException("stream is not ready to ready");
+            }
+
+            var totalBytesRead = 0;
+
+            var ret = new StringBuilder();
+
+            byte[] buffer = new byte[bufferSize];
+            var bytesRead = await stream.ReadAsync(buffer, 0, bufferSize);
+
+            while (bytesRead > 0)
+            {
+                totalBytesRead += bytesRead;
+
+                if (totalBytesRead > maxSize)
+                {
+                    throw new InvalidOperationException($"stream response size is greter than {maxSize}, url = {loggingUrl}, content = {loggingStrContent}");
+                }
+
+                ret.Append(encoding.GetString(bytes: buffer, index: 0, count: bytesRead));
+
+                bytesRead = await stream.ReadAsync(buffer, 0, bufferSize);
+
+            }
+
+            return ret.ToString();
+        }
+
+        private string GetStringContentForLoggingFromHeaders(AuthenticationHeaderValue authenticationHeaderValue,
+            List<KeyValuePair<string, string>> headerValues)
+        {
+            var ret = new List<string>();
+
+            if (authenticationHeaderValue != null)
+            {
+                ret.Add($"authenticationHeader: {{ scheme: \"{authenticationHeaderValue.Scheme}\", parameter: \"{authenticationHeaderValue.Parameter}\" }}");
+            }
+
+            if (headerValues != null && headerValues.Any() == true)
+            {
+                var retHeaders = new List<string>();
+
+                foreach (var iHeaderValue in headerValues)
+                {
+                    retHeaders.Add($"{{ key: \"{iHeaderValue.Key}\", value: \"{iHeaderValue.Value}\" }}");
+                }
+
+                ret.Add($"headers: [{string.Join(", ", retHeaders)}]");
+            }
+
+            return string.Join(", ", ret);
         }
 
         #endregion
