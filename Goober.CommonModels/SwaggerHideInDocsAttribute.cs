e00850040d2e163e0e26af2c1aa0880dd1d51b41
Denis Tskhay <denis.tskhaj@yandex.ru>
2021-04-15 11:02:17 +0300

    fix

--- Goober.CommonModels/SwaggerHideInDocsAttribute.cs
+++ Goober.CommonModels/SwaggerHideInDocsAttribute.cs
@@ -5,16 +5,13 @@ namespace Goober.CommonModels
     [AttributeUsage(AttributeTargets.Method)]
     public class SwaggerHideInDocsAttribute : Attribute
     {
-        public SwaggerHideInDocsAttribute()
-        {
-
-        }
-
-        public SwaggerHideInDocsAttribute(string password)
+        public SwaggerHideInDocsAttribute(string cookieName = "swagger-show", string password = "password")
         {
             Password = password;
+            CookieName = cookieName;
         }
 
         public string Password { get; set; } = "password";
+        public string CookieName { get; set; } = "swagger-show";
     }
 }
